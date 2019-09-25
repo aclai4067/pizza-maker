ff254dc53890047af8fe61d9558a250a60fe3e60
Ashley Claiborne <languageisaliquid@gmail.com>
2019-09-25 00:00:08 -0500

    printed totals to dom on click

--- javascripts/components/order.js
+++ javascripts/components/order.js
@@ -1,18 +1,34 @@
 import cheese from './cheese.js';
+// import meat from './meat.js';
+// import veggie from './veggie.js';
 import utilities from '../helpers/utilities.js';
 
 const createFinalOrder = (toppings) => {
-    let toppingString = '';
+    let toppingString = `<h2 id='summary' class='text-center'>Your Order Summary</h2>`;
+    let total = 0;
     for(let i = 0; i < toppings.length; i++){
+        total += toppings[i].price;
         toppingString += `
-        <h2>${toppings[i].type}</h2>
+        <div class='row d-flex'>
+        <h4 class='col-6 text-center'>${toppings[i].type}</h4>
+        <h4 class='col-6 text-center'>$${(toppings[i].price).toFixed(2)}</h4>
+        </div>
         `
     }
+    toppingString += `
+        <div id='totalPrice' class='row d-flex'>
+        <h3 class='col-6 text-center'>Total:</h3>
+        <h3 class='col-6 text-center'>$${total.toFixed(2)}</h3>
+        </div>
+    `
     utilities.printToDom('final-order', toppingString)
 };
 
 const createOrderEvent = () => {
     const selectedCheeses = cheese.getSelectedCheeses();
+    // const selectedMeats = meat.getSelectedMeats();
+    // const selectedVeggies = veggies.getSelectedVeggies();
+    // const allToppings = selectedCheeses.concat(selectedMeats, selectedVeggies);
     createFinalOrder(selectedCheeses);
 };
 
